ebc83dc7c4d7ab209529af56ec111e8e6836ffa8
Antony Kalogeropoulos <anthonykalogeropoulos@gmail.com>
2016-08-11 16:20:57 +0300

    Change env to production

--- public/js/zapitqr.js
+++ public/js/zapitqr.js
@@ -3,12 +3,12 @@
  */
 // function zapitqr() {
 
-/** Local Testing **/
+// /** Local Testing **/
+// document.getElementById("zapitqr").innerHTML =
+//     '<iframe seamless="seamless" style="margin:0; padding:0; border:0; width: 120px;" id="iframeqr" src="zapit.app/showWidget"></iframe>';
+
+/** Heroku **/
 document.getElementById("zapitqr").innerHTML =
     '<iframe seamless="seamless" style="margin:0; padding:0; border:0; width: 120px;" id="iframeqr" src="https://zapit-api.herokuapp.com/showWidget"></iframe>';
-
-// /** Heroku **/
-// document.getElementById("zapitqr").innerHTML =
-//     '<iframe seamless="seamless" style="margin:0; padding:0; border:0; width: 120px;" id="iframeqr" src="/showWidget"></iframe>';
 
 // }
