3925982f33208ec4df64cc3298491e1eaeafc2c3
Antony Kalogeropoulos <anthonykalogeropoulos@gmail.com>
2016-08-11 16:03:42 +0300

    Change everything to relative urls so that they work both locally and with heroku | Composer update

--- public/js/zapitqr.js
+++ public/js/zapitqr.js
@@ -3,12 +3,12 @@
  */
 // function zapitqr() {
 
-    // document.getElementById("zapitqr").innerHTML =
-    //     '<div style="position: relative; top: 10px; width: 100%; color: green; background: yellow; text-align: center;">Zapit!' +
-    //     '</div>' +
-    //     '<img src="http://zapit.app/"/>';
-
+/** Local Testing **/
 document.getElementById("zapitqr").innerHTML =
-    '<iframe seamless="seamless" style="margin:0; padding:0; border:0; width: 120px;" id="iframeqr" src="http://zapit.app/showWidget"></iframe>';
+    '<iframe seamless="seamless" style="margin:0; padding:0; border:0; width: 120px;" id="iframeqr" src="/showWidget"></iframe>';
+
+// /** Heroku **/
+// document.getElementById("zapitqr").innerHTML =
+//     '<iframe seamless="seamless" style="margin:0; padding:0; border:0; width: 120px;" id="iframeqr" src="/showWidget"></iframe>';
 
 // }
