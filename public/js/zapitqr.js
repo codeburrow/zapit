da61694361959f141f02791ab171c973e8bdde6b
Antony Kalogeropoulos <anthonykalogeropoulos@gmail.com>
2016-08-11 16:11:10 +0300

    Remake url absolute because heroku didn't understand it

--- public/js/zapitqr.js
+++ public/js/zapitqr.js
@@ -5,7 +5,7 @@
 
 /** Local Testing **/
 document.getElementById("zapitqr").innerHTML =
-    '<iframe seamless="seamless" style="margin:0; padding:0; border:0; width: 120px;" id="iframeqr" src="/showWidget"></iframe>';
+    '<iframe seamless="seamless" style="margin:0; padding:0; border:0; width: 120px;" id="iframeqr" src="https://zapit-api.herokuapp.com/showWidget"></iframe>';
 
 // /** Heroku **/
 // document.getElementById("zapitqr").innerHTML =
